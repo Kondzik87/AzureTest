33593c340365dbab7bfa83a72860ca9420a84f31
1308499 <kondzik.u@gmail.com>
2015-10-19 15:35:58 +0100

    ....

--- index.php
+++ index.php
@@ -21,6 +21,18 @@
        and neither do I */
 
     ?>
+
+    <?php
+    $name = "Edgar";
+
+    if ($name == "Simon") {
+        print "I know you!";
+    }
+    else {
+        print "Who are you?";
+    }
+    ?>
+
 </p>
 
 </body>
